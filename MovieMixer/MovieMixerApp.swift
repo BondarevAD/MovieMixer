dfc895e3c6d1849bb105a92d4941880f7d6472a5
Andrew Bondarev <andreybondarev48@gmail.com>
2023-04-24 13:06:35 +0300

    Initial Commit

---
+++ MovieMixer/MovieMixerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieMixerApp.swift
+//  MovieMixer
+//
+//  Created by User on 24.04.23.
+//
+
+import SwiftUI
+
+@main
+struct MovieMixerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
